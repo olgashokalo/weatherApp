9547f307e5f31fe5ae6a57f8b549bf1c3c91c243
Olga Shokalo <olha.shokalo@gmail.com>
2023-03-06 12:50:04 +0100

    Made the code more "readable"

--- src/index.js
+++ src/index.js
@@ -38,71 +38,6 @@ function formatDate(timestamp) {
   return `${Day},${Month} ${now.getDate()}, ${hour}:${minute}`;
 }
 
-function getForecast(coords) {
-  let apiKey = "3632a7c9224763143fe6obtb61dff025";
-  let apiUrl = ` https://api.shecodes.io/weather/v1/forecast?lon=${coords.longitude}&lat=${coords.latitude}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayForecast);
-}
-function showWeather(response) {
-  celsiusTemperature = response.data.temperature.current;
-  document.querySelector("#currentCityName").innerHTML = response.data.city;
-  document.querySelector("#currentTemperature").innerHTML =
-    Math.round(celsiusTemperature);
-  document.querySelector("#feels-like").innerHTML = Math.round(
-    response.data.temperature.feels_like
-  );
-  document.querySelector("#humidity").innerHTML = Math.round(
-    response.data.temperature.humidity
-  );
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-  document.querySelector("#icon-description").innerHTML =
-    response.data.condition.description;
-  document.querySelector("#currentTime").innerHTML = formatDate(
-    response.data.time * 1000
-  );
-  document
-    .querySelector("#icon-now")
-    .setAttribute("src", response.data.condition.icon_url);
-
-  document
-    .querySelector("#icon-now")
-    .setAttribute("alt", response.data.condition.description);
-
-  getForecast(response.data.coordinates);
-}
-
-function search(city) {
-  let apiKey = "3632a7c9224763143fe6obtb61dff025";
-  let units = "metric";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(showWeather);
-}
-function LetUsGo(event) {
-  event.preventDefault();
-  let city = document.querySelector("#input-city").value;
-  search(city);
-}
-
-let cit = document.querySelector("#input-form");
-cit.addEventListener("submit", LetUsGo);
-
-function searchPosition(position) {
-  let apiKey = "3632a7c9224763143fe6obtb61dff025";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${position.coords.longitude}&lat=${position.coords.latitude}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showWeather);
-}
-
-function showCurrentPosition(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchPosition);
-}
-
-let celsiusTemperature = null;
-let curLocRes = document.querySelector("#currentLocationResult");
-curLocRes.addEventListener("click", showCurrentPosition);
-
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
@@ -146,4 +81,69 @@ function displayForecast(response) {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
+
+function getForecast(coords) {
+  let apiKey = "3632a7c9224763143fe6obtb61dff025";
+  let apiUrl = ` https://api.shecodes.io/weather/v1/forecast?lon=${coords.longitude}&lat=${coords.latitude}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function showWeather(response) {
+  celsiusTemperature = response.data.temperature.current;
+  document.querySelector("#currentCityName").innerHTML = response.data.city;
+  document.querySelector("#currentTemperature").innerHTML =
+    Math.round(celsiusTemperature);
+  document.querySelector("#feels-like").innerHTML = Math.round(
+    response.data.temperature.feels_like
+  );
+  document.querySelector("#humidity").innerHTML = Math.round(
+    response.data.temperature.humidity
+  );
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#icon-description").innerHTML =
+    response.data.condition.description;
+  document.querySelector("#currentTime").innerHTML = formatDate(
+    response.data.time * 1000
+  );
+  document
+    .querySelector("#icon-now")
+    .setAttribute("src", response.data.condition.icon_url);
+
+  document
+    .querySelector("#icon-now")
+    .setAttribute("alt", response.data.condition.description);
+
+  getForecast(response.data.coordinates);
+}
+function searchPosition(position) {
+  let apiKey = "3632a7c9224763143fe6obtb61dff025";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${position.coords.longitude}&lat=${position.coords.latitude}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function showCurrentPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchPosition);
+}
+let curLocRes = document.querySelector("#currentLocationResult");
+curLocRes.addEventListener("click", showCurrentPosition);
+
+function search(city) {
+  let apiKey = "3632a7c9224763143fe6obtb61dff025";
+  let units = "metric";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function LetUsGo(event) {
+  event.preventDefault();
+  let city = document.querySelector("#input-city").value;
+  search(city);
+}
+
+let cit = document.querySelector("#input-form");
+cit.addEventListener("submit", LetUsGo);
+
 search("London");
